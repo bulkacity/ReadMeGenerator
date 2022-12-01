ce984b0c923000c69b1909e728cf6d6e0f882116
bulkacity <112503952+bulkacity@users.noreply.github.com>
2022-11-30 21:47:21 -0500

    Added the section of questions

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+/* the following code will be replaced by await function */
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    }
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+//----------------------------------------------------------------
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     }
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+//----------------------------------------------------------------
+
+
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type:"input"
-  }
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
+  {
+    type:"input",
+    message: "Enter youre project title",
+    name:"title",
+  },
 ];
 
 // TODO: Create a function to write README file
